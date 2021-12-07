abdb947eb8ed02c23d5cee02f10a34b8f0dd8b65
Tim Veletta <timothy.veletta@gmail.com>
2021-12-08 00:09:34 +0800

    Fix build failure for importing next/document in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import React from 'react'
-import { Head } from 'next/document'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <div className="container">
-    <main className="main">
-      <Component {...pageProps} />
-    </main>
-    <footer className="footer">
-        <p>Made with &#x2764; by <a href="https://www.timveletta.com/">Tim Veletta</a></p>
-    </footer>
-  </div>
+  return (
+    <div className="container">
+      <main className="main">
+        <Component {...pageProps} />
+      </main>
+      <footer className="footer">
+        <p>
+          Made with &#x2764; by{" "}
+          <a href="https://www.timveletta.com/">Tim Veletta</a>
+        </p>
+      </footer>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
